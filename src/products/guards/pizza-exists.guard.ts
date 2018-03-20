c8e6b3f2764ae9bbf97a2f421fe6e77faf460240
Abin Punnoose <punnoose.abin@gmail.com>
2018-03-20 17:08:04 +0530

    28: PizzaExists Guard

--- src/products/guards/pizza-exists.guard.ts
+++ src/products/guards/pizza-exists.guard.ts
@@ -4,7 +4,7 @@ import { CanActivate, ActivatedRouteSnapshot } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-import { tap, filter, take, catchError, switchMap } from 'rxjs/operators';
+import { tap, map, filter, take, catchError, switchMap } from 'rxjs/operators';
 
 import * as fromStore from '../store';
 import { Pizza } from '../models/pizza.model';
@@ -15,17 +15,19 @@ export class PizzaExistsGuard implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
     return this.checkStore().pipe(
-        switchMap(() => {
-            const id = parseInt(route.params.pizzaId, 10);
-            return this.hasPizza(id);
-        })
+      switchMap(() => {
+        const id = parseInt(route.params.pizzaId, 10);
+        return this.hasPizza(id);
+      })
     );
   }
 
   hasPizza(id: number): Observable<boolean> {
-      return this.store.select(fromStore.getPizzasEntities).pipe(
-          map((entities: { [key: number]: Pizza}) => !!entities[id]),
-          take(1)
+    return this.store
+      .select(fromStore.getPizzasEntities)
+      .pipe(
+        map((entities: { [key: number]: Pizza }) => !!entities[id]),
+        take(1)
       );
   }
 
